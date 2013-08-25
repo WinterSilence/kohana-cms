2b245b167785b35c2ad1c9f33a2eb6613e506ee0
WinterSilence <info@handy-soft.ru>
2013-08-25 19:45:05 +0400

    first

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Entry point for backend application
+ *
+ */
+$application = 'backend'; // Application name\directory
+$app_alias   = 'admin/';  // Application alias, use in URL`s
+
+require_once 'index.php';
