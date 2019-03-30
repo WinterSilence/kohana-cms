be7fae88b2e1d0c9febb49c6aba8bac084842f31
Anton <antonfedonjuk@gmail.com>
2019-03-30 23:43:59 +0300

    Update init.php

--- common/init.php
+++ common/init.php
@@ -1,17 +1,15 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-// Smarty engine autoloader
-function smarty_autoload($class)
-{
-	if ($class == 'Smarty' OR $class == 'SmartyBC')
-	{
-		require_once 'vendor'.DS.'smarty'.DS.$class.'.class'.EXT;
-	}
-}
-spl_autoload_register('smarty_autoload');
+<?php
 
 // Path to common dir
 if ( ! defined('COMMONPATH'))
 {
 	define('COMMONPATH', __DIR__.DS);
 }
+
+// Smarty engine autoloader
+spl_autoload_register(function ($class) {
+	if ($class == 'Smarty' OR $class == 'SmartyBC')
+	{
+		require_once COMMONPATH.'vendor'.DS.'smarty'.DS.$class.'.class'.EXT;
+	}
+});
